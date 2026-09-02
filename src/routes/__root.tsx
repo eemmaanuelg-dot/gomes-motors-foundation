d5962ea7578dd6380b101cd17dda3c1020fb7e58
eemmaanuelg-dot <eemmaanuelg@gmail.com>
2026-09-02 01:57:13 -0300

    fix: bind favicon to local brand mark

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,6 +10,7 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
+import favicon from "../assets/gomes-motors-mark.svg";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Header } from "../components/site/Header";
 import { Footer } from "../components/site/Footer";
@@ -97,7 +98,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/brand/gomes-motors-mark.svg", type: "image/svg+xml" },
+      { rel: "icon", href: favicon, type: "image/svg+xml" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
